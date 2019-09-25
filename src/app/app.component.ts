390ba28c4487c47e56172cde6aae68cedafae928
akaram <akarampadolkar@gmail.com>
2019-09-25 18:18:26 +0530

    Added doc folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component , OnInit} from '@angular/core';
 import { PwaServiceService } from './pwa-service.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,36 @@ import { PwaServiceService } from './pwa-service.service';
 export class AppComponent implements OnInit{
 
   title = 'myApp';
+  todos = [];
+
+  addForm = new FormGroup({
+    name: new FormControl('')
+  })
+
   constructor(public Pwa: PwaServiceService) {}
   installPwa(): void {
     this.Pwa.promptEvent.prompt();
   }
 
   ngOnInit() {
+    this.getTodo();
   }
   
+  getTodo(){
+    this.todos = JSON.parse(localStorage.getItem('todolist'));
+  }
+  submit(){
+    let oldItems = JSON.parse(localStorage.getItem('todolist')) || [];
+    oldItems.push(this.addForm.value);
+
+    localStorage.setItem('todolist', JSON.stringify(oldItems));
+
+    //call method for add recenr todo in list 
+    this.getTodo();
+    this.addForm.reset();
+  }
+
+  // deleteTodo(deletingTodo){
+  //   console.log(deletingTodo)
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+
+import { AlertModule } from 'ngx-bootstrap/alert';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,8 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    AlertModule.forRoot(),
     ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
